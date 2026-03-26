a46c9df1fe80153dc1ce0104a8f0344cbbdcc701
Lacilo <szaszfail@kkszki.hu>
2026-03-26 13:19:19 +0100

    .

--- Snake/Controller/SnakeController.cs
+++ Snake/Controller/SnakeController.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Snake.Models;
 
 namespace Snake.Controller
 {
     internal class SnakeController
     {
-
+        public static void AppendSnake(SnakePos snake, char direction)
+        {
+            switch (direction)
+            {
+                case 'w':
+                    snake.Positions.Add(new Pos(snake.Positions.Last().X, snake.Positions.Last().Y - 1));
+                    break;
+                case 's':
+                    snake.Positions.Insert(0, new Pos(snake.Positions[0].X, snake.Positions[0].Y + 1));
+                    Console.WriteLine("Hosszabbítottam a kígyót");
+                    snake.Positions.ForEach(x => Console.WriteLine(x.X + " " + x.Y));
+                    break;
+            }
+        }
     }
 }
--- Snake/Program.cs
+++ Snake/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using Snake.Controller;
 using Snake.Models;
 using Snake.View;
 
@@ -10,10 +10,15 @@ namespace MyApp
         static void Main(string[] args)
         {
             SnakeView view = new SnakeView();
-            SnakePos snake = new SnakePos() {
-                Positions = new List<Pos> { new Pos(0, 0), new Pos(1, 0), new Pos(2, 0), new Pos(3, 0), new Pos(4, 0)}
+            SnakePos snake = new SnakePos()
+            {
+                Positions = new List<Pos> { new Pos(0, 0), new Pos(1, 0), new Pos(2, 0), new Pos(3, 0), new Pos(4, 0) }
             };
 
+            view.InsertSnakeIntoMap(snake);
+            view.DisplayMap();
+
+            SnakeController.AppendSnake(snake, 's');
             view.InsertSnakeIntoMap(snake);
             view.DisplayMap();
         }
