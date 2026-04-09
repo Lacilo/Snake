6d05ce35526161224468dd91be947ddec55644b9
AdamNandor <adamn@kkszki.hu>
2026-04-09 23:36:46 +0200

    GetLoginList

--- Snake/Controller/UserController.cs
+++ Snake/Controller/UserController.cs
@@ -11,6 +11,36 @@ namespace Snake.Controller
 {
     internal class UserController
     {
+        public List<Login> GetLoginList()
+        {
+            try
+            {
+                MySqlConnection conn = new MySqlConnection("server=localhost;user=root;password=;database=snake;");
+                conn.Open();
+                string comd = "SELECT * FROM bejelentkezes;";
+                MySqlCommand cmd = new MySqlCommand(comd, conn);
+                List<Login> connections = new List<Login>();
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        connections.Add(new Login(
+                            reader.GetString("FelhNev"),
+                            reader.GetString("Jelszo")
+                        ));
+                    }
+
+                    conn.Close();
+                    return connections;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
         
     }
 }
