c96a1bbeca5257475b0b7958ee099f8c0599da08
AdamNandor <adamn@kkszki.hu>
2026-04-09 23:37:56 +0200

    RegistrationView

--- Snake/View/LoginView.cs
+++ Snake/View/LoginView.cs
@@ -10,6 +10,34 @@ namespace Snake.View
 {
     internal class LoginView
     {
+        static void RegistrationView()
+        {
+            bool b = true;
+            while (b)
+            {
+                Console.Clear();
+                Console.WriteLine("=== REGISZTRÁCIÓ ===");
+
+                Console.WriteLine("Felhasználónév: ");
+                string felhNev = Console.ReadLine();
+
+                Console.WriteLine("Jelszó: ");
+                string jelszo = Console.ReadLine();
+
+                try
+                {
+                    new UserController().NewRegistration(felhNev,jelszo);
+                    Console.WriteLine("Sikeres regisztráció!");
+                    Console.ReadLine();
+                    b = false;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }        
+            }
+        }
+
         
     }
 }
