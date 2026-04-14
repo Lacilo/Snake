4588bf8c99b910b21f906a7c14b94610399bf7c5
lacilo <szaszfail@kkszki.hu>
2026-04-14 20:41:56 +0200

    .

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -63,7 +63,26 @@ namespace MyApp
                     }
                     else
                     {
-                        MenuView.DisplayMainMenu(ref currentUser);
+                        // Bejelentkezés/regisztráció
+                        Console.Clear();
+                        Console.WriteLine("Bejelentkezés - 1 ");
+                        Console.WriteLine("Regisztráció - 2");
+                        Console.WriteLine("Kilépés - 3");
+
+                        string choice = Console.ReadLine();
+
+                        if (choice == "1") 
+                        {
+                            currentUser = LoginView.BejelentkezesView();
+                        }
+                        else if(choice == "2")
+                        {
+                            LoginView.RegistrationView();
+                        }
+                        else if(choice == "3")
+                        {
+                            Environment.Exit(0);
+                        }
                     }
                 }
                 catch (IndexOutOfRangeException iorEx)
